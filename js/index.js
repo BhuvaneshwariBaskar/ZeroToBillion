2bb139c64c4066fced89dbff065bdba5341a2bac
solomonsanthosh <solosanty2064@gmail.com>
2024-04-29 11:21:11 -0700

    SS

--- js/index.js
+++ js/index.js
@@ -6,6 +6,37 @@
  *   Webflow.push(readyFunction);
  */
 
+let navbar = document.querySelector(".navbar");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+	console.log("GG")
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+// let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+// htmlcssArrow.onclick = function() {
+// navLinks.classList.toggle("show1");
+// }
+// let moreArrow = document.querySelector(".more-arrow");
+// moreArrow.onclick = function() {
+// navLinks.classList.toggle("show2");
+// }
+// let jsArrow = document.querySelector(".js-arrow");
+// jsArrow.onclick = function() {
+// navLinks.classList.toggle("show3");
+// }
 ;(() => {
 	var kJ = Object.create
 	var Uu = Object.defineProperty
